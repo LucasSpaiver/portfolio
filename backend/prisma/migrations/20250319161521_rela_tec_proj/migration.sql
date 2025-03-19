35b267b09015e5ca99af3a6d3dc726d375d1efaa
Lucas Spavier <lucaspavier@gmail.com>
2025-03-19 13:17:46 -0300

    Segundo commit

---
+++ backend/prisma/migrations/20250319161521_rela_tec_proj/migration.sql
@@ -0,0 +1,19 @@
+-- DropForeignKey
+ALTER TABLE "projetos" DROP CONSTRAINT "projetos_tecnologiaId_fkey";
+
+-- CreateTable
+CREATE TABLE "_ProjetoToTecnologia" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_ProjetoToTecnologia_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_ProjetoToTecnologia_B_index" ON "_ProjetoToTecnologia"("B");
+
+-- AddForeignKey
+ALTER TABLE "_ProjetoToTecnologia" ADD CONSTRAINT "_ProjetoToTecnologia_A_fkey" FOREIGN KEY ("A") REFERENCES "projetos"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProjetoToTecnologia" ADD CONSTRAINT "_ProjetoToTecnologia_B_fkey" FOREIGN KEY ("B") REFERENCES "tecnologias"("id") ON DELETE CASCADE ON UPDATE CASCADE;
